6b8bfb6bf68023ce4eaa8917deab2da12b3f2592
Josh McArthur <joshua.mcarthur@gmail.com>
2019-08-28 21:23:59 +1200

    Use split view with map

--- .env
+++
@@ -1,1 +0,0 @@
-export SASS_PATH=./node_modules
--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "./App.scss";
-import Card, { CardPrimaryContent } from "@material/react-card";
-import '@material/react-card/index.scss';
-import '@material/react-typography/index.scss';
+import "@material/react-top-app-bar/index.scss";
+import "@material/react-list/index.scss";
+import "@material/react-drawer/index.scss";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-import { Headline6, Subtitle2 } from "@material/react-typography";
 import routeGeoJSON from "./data/route_and_waypoints";
+
+import TopAppBar, {
+  TopAppBarFixedAdjust,
+  TopAppBarRow,
+  TopAppBarSection,
+  TopAppBarTitle
+} from "@material/react-top-app-bar";
+
+import Drawer, {
+  DrawerAppContent,
+  DrawerContent,
+  DrawerHeader,
+  DrawerSubtitle,
+  DrawerTitle
+} from "@material/react-drawer";
+
+import List, {
+  ListItem,
+  ListItemText,
+} from "@material/react-list";
+
+import ReactMapGL from 'react-map-gl';
 
 const deriveSections = routeAndWaypoints => {
   // Anything that is a linestring is a route segment
@@ -19,43 +41,104 @@ const deriveSections = routeAndWaypoints => {
   // Each route should have a start and end waypoint.
   return routeParts.map(part => ({
     start: waypoints.find(
-      wp => wp.geometry.coordinates.toString() === part.geometry.coordinates[0].toString()
+      wp =>
+        wp.geometry.coordinates.toString() ===
+        part.geometry.coordinates[0].toString()
     ),
     finish: waypoints.find(
       wp =>
         wp.geometry.coordinates.toString() ===
-        part.geometry.coordinates[part.geometry.coordinates.length - 1].toString()
+        part.geometry.coordinates[
+          part.geometry.coordinates.length - 1
+        ].toString()
     ),
     segment: part
   }));
 };
 
-const SectionCard = ({ section }) => {
+const SectionListItem = ({ section }) => {
   let title;
-  if (section.start && section.finish) title = `${section.start.properties.name} to ${section.finish.properties.name}`
-  else if (section.finish) title = `Start -> ${section.finish.properties.name}`
-  else if (section.start) title = `${section.start.properties.name} -> Finish`
+  if (section.start && section.finish)
+    title = `${section.start.properties.name} to ${section.finish.properties.name}`;
+  else if (section.finish) title = `Start -> ${section.finish.properties.name}`;
+  else if (section.start) title = `${section.start.properties.name} -> Finish`;
 
   return (
-    <Card className="SectionCard" outlined>
-      <CardPrimaryContent>
-        <div>
-          <Headline6>{title}</Headline6>
-          <Subtitle2>{section.segment.properties.desc}</Subtitle2>
-        </div>
-      </CardPrimaryContent>
-    </Card>
+    <ListItem>
+      <ListItemText
+        primaryText={title}
+        secondaryText={section.segment.properties.desc}
+      />
+    </ListItem>
   );
 };
 
-class App extends React.Component {
-  state = { sections: deriveSections(routeGeoJSON.features) };
+class Map extends React.Component {
+
+  state = {
+    viewport: {
+      width: "100%",
+      height: "100%",
+      latitude: -42,
+      longitude: 174,
+      zoom: 8
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        {this.state.sections.map(s => (
-          <SectionCard section={s} key={s.segment.properties.cmt} />
-        ))}
+      <ReactMapGL
+        {...this.state.viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_KEY}
+        onViewportChange={(viewport) => this.setState({viewport})}
+      />
+    );
+  }
+}
+
+
+class App extends React.Component {
+  state = { sections: deriveSections(routeGeoJSON.features), selectedIndex: 0 };
+  render() {
+    return (
+      <div className="drawer-container">
+        <TopAppBar fixed>
+          <TopAppBarRow>
+            <TopAppBarSection align="start">
+              <TopAppBarTitle>Te Araroa Progress Meter</TopAppBarTitle>
+            </TopAppBarSection>
+          </TopAppBarRow>
+        </TopAppBar>
+
+        <TopAppBarFixedAdjust className="top-app-bar-fix-adjust">
+          <Drawer style={{ width: "33%" }}>
+            <DrawerHeader>
+              <DrawerTitle tag="h2">Sections</DrawerTitle>
+              <DrawerSubtitle></DrawerSubtitle>
+            </DrawerHeader>
+            <DrawerContent>
+              <List
+                singleSelection
+                twoLine
+                selectedIndex={this.state.selectedIndex}
+                handleSelect={selectedIndex =>
+                  this.setState({ selectedIndex })
+                }
+              >
+                {this.state.sections.map(s => (
+                  <SectionListItem
+                    key={s.segment.properties.cmt}
+                    section={s}
+                  />
+                ))}
+              </List>
+            </DrawerContent>
+          </Drawer>
+
+          <DrawerAppContent className="drawer-app-content">
+            <Map />
+          </DrawerAppContent>
+        </TopAppBarFixedAdjust>
       </div>
     );
   }
