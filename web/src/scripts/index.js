dd6d0771a783aa1f16a61ef42478f6a3f5e9373f
Josh McArthur <joshua.mcarthur@gmail.com>
2019-08-30 22:02:55 +1200

    Start adding simple web interface

---
+++ web/src/scripts/index.js
@@ -0,0 +1,34 @@
+import '../styles/index.scss';
+import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
+import jQuery from "jquery";
+mapboxgl.accessToken = "pk.eyJ1Ijoic3Vkb2pvc2giLCJhIjoiY2p6djFpa2o0MGJlNDNibXIydjc1azcxNCJ9.4eH1qtw74O1soNIExDKHzQ";
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v9',
+  zoom: 6,
+  center: [174, -42]
+});
+
+
+jQuery.ajax({
+    url: "https://script.google.com/macros/s/AKfycbzGvKKUIaqsMuCj7-A2YRhR-f7GZjl4kSxSN1YyLkS01_CfiyE/exec",
+ 
+    // The name of the callback parameter, as specified by the YQL service
+    jsonp: "callback",
+ 
+    // Tell jQuery we're expecting JSONP
+    dataType: "jsonp",
+ 
+    // Tell YQL what we want and that we want JSON
+    data: {
+        id: "1ddAdoHp2q0yzX_cR5NX_fMsiJ0uA9KYHgqGpF9gqnHg",
+        sheet: "output"
+    },
+ 
+    // Work with the response
+    success: function( response ) {
+        console.log( response ); // server response
+    }
+});
